c3cd4fea02971f92e5b9086ab1eb94494608b6da
kaki005 <u097498f@ecs.osaka-u.ac.jp>
2026-01-27 14:54:01 +0900

    update rooturl

--- src/settings.ts
+++ src/settings.ts
@@ -4,7 +4,7 @@ export const profile = {
 	institute: 'SANKEN, The university of Osaka',
 	author_name: 'Soshi Kakio', // Author name to be highlighted in the papers section
 	research_areas: [
-		// { title: 'Physics', description: 'Brief description of the research interest', field: 'physics' },
+		{ title: 'Physics', description: 'Brief description of the research interest', field: 'physics' },
 	],
 }
 
@@ -23,7 +23,8 @@ export const social = {
 }
 
 export const template = {
-	website_url: 'https://localhost:4321', // Astro needs to know your site’s deployed URL to generate a sitemap. It must start with http:// or https://
+	// website_url: 'https://localhost:4321', // Astro needs to know your site’s deployed URL to generate a sitemap. It must start with http:// or https://
+	website_url: 'https://kaki005.github.io/astro_academia/', // Astro needs to know your site’s deployed URL to generate a sitemap. It must start with http:// or https://
 	menu_left: false,
 	transitions: true,
 	lightTheme: 'light', // Select one of the Daisy UI Themes or create your own
