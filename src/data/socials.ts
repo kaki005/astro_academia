1706af1641b6915bf691e77856f22bc3eebc7965
kaki005 <u097498f@ecs.osaka-u.ac.jp>
2026-01-29 16:33:52 +0900

    modify ui

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -14,4 +14,5 @@ export const SOCIALS: LinkedIcon[] = [
     new LinkedIcon("linkedln", "https://www.linkedin.com/feed/", "linkedin", "#0A66C2"),
     new LinkedIcon("github", "https://github.com/kaki005", "github"),
     new LinkedIcon("orcid", "https://orcid.org/my-orcid?orcid=0009-0006-6200-3858", "orcid", "#A6CE39"),
+    new LinkedIcon("Labo", "https://www.dm.sanken.osaka-u.ac.jp/", "school"),
 ]
