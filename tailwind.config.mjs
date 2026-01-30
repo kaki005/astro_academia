2a6e88d8af6db68e9f185290c8c455150fa293c1
kaki005 <u097498f@ecs.osaka-u.ac.jp>
2026-01-30 23:28:04 +0900

    add sidebar

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	darkMode: ["class"],
-	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	content: [
+		"./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+		"./node_modules/flowbite/**/*.js"
+	],
 	theme: {
 		extend: {
 			borderRadius: {
