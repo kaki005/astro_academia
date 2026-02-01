19f7d4f972bef9f4e46e462179449135b266387f
kaki005 <u097498f@ecs.osaka-u.ac.jp>
2026-02-01 12:40:02 +0900

    modify rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,14 +1,13 @@
 import rss from "@astrojs/rss";
-import enMessages from '../messages/en'
 import { getCollection } from "astro:content";
 
 export async function GET(context) {
     const blog = await getCollection("blog");
     return rss({
         // `<title>` field in output xml
-        title: enMessages.SEO.default_title,
+        title: "Soshi Kakio",
         // `<description>` field in output xml
-        description: enMessages.SEO.default_description,
+        description: "Soshi Kakio's personal websites.",
         // Pull in your project "site" from the endpoint context
         // https://docs.astro.build/en/reference/api-reference/#site
         site: context.site,
