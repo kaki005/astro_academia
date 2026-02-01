5aacb7b491eb91fac7432bf424f2b4bd1751ad4a
kaki005 <u097498f@ecs.osaka-u.ac.jp>
2026-02-01 18:40:30 +0900

    fix npm dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
-import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 import { template } from "./src/settings";
@@ -9,7 +7,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react(), tailwind(), sitemap()],
+    integrations: [tailwind(), sitemap()],
     site: template.website_url,
     base: template.base,
     i18n: {
