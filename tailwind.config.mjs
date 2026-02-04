139e9235420466ca9e328ca79a92920313bfee93
kaki005 <u097498f@ecs.osaka-u.ac.jp>
2026-02-04 11:21:01 +0900

    remove flowbite

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,7 @@ export default {
 	darkMode: ["class"],
 	content: [
 		"./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
-		"./node_modules/flowbite/**/*.js"
+		// "./node_modules/flowbite/**/*.js"
 	],
 	theme: {
 		extend: {
@@ -56,7 +56,7 @@ export default {
 			}
 		}
 	},
-	plugins: [require("@tailwindcss/typography"), require("daisyui"), require("tailwindcss-animate"), require("flowbite/plugin")],
+	plugins: [require("@tailwindcss/typography"), require("daisyui"), require("tailwindcss-animate")],
 	daisyui: {
 		themes: [
 			"light",
