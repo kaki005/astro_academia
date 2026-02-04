9ee04ac7a7c6bbd92622912e159e28c7bc3e80f7
kaki005 <u097498f@ecs.osaka-u.ac.jp>
2026-02-04 16:26:23 +0900

    remove astrojs/tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import { template } from "./src/settings";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), sitemap()],
+    integrations: [sitemap()],
     site: template.website_url,
     base: template.base,
     i18n: {
